3ef6023168a3e23f1e63aaf56d640a26f8de8ce2
Enzo Huynh <enzo_huynh@hotmail.com>
2018-11-27 16:52:57 +0100

    done

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,10 +1,14 @@
 function type_check_v1(arg1, arg2) {
 
-    if (typeof(arg1) === typeof(arg2) || typeof(arg2) === null || typeof(arg1) === 'object' ) {
+    if (typeof(arg1) === typeof(arg2) || typeof(arg2) === 'object') {
         return true;
-    } else if (typeof(arg1) && typeof(arg2)) {
+    } else if (typeof(arg1) === null && typeof(arg2) === 'null') {
        return true;
-    } else {
+    } else if (typeof(arg1) && typeof(arg2)) {
+        return true;
+    } else if (Array.isArray(arg1) && typeof(arg2) === 'array') {
+        return true;
+    }else {
         return false;
     }
 }
